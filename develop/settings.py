2859a138f808a1c1c5d8ca18dd617c52900a358f
masami <masami@syks@theia.ocn.ne.jp>
2021-11-23 23:29:14 +0900

    postgre

--- develop/settings.py
+++ develop/settings.py
@@ -75,13 +75,23 @@ WSGI_APPLICATION = 'develop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'd78qtgmr3j0v20',
+        'HOST': 'ec2-3-92-15-1.compute-1.amazonaws.com',
+        'PORT': 5432,
+        'USER': 'uaqbqddanjihuh',
+        'PASSWORD': 'a93164578b668292ed34679490b972cd0eb8e6b6d86be3bb07be1ee173fdfa4d',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
